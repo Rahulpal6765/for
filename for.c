32135e907333e2d6945b0b17263a3fb68ed79049
Rahulpal6765 <165860634+Rahulpal6765@users.noreply.github.com>
2024-04-03 07:33:48 +0530

    Add files via upload

---
+++ for.c
@@ -0,0 +1,14 @@
+// # include<stdio.h>
+// int main() {
+//     for(int i = 1; i<=7; i++){
+//         printf("Hello World\n");
+//     }
+//     return 0;
+// }
+# include<stdio.h>
+int main() {
+    for(char ch = 'a'; ch<='z'; ch++){
+        printf("%c ", ch);
+    }
+    return 0;
+}
